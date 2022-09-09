7298202273fbd0fc35475ec7185d2539a88490b0
rominamarmontel <romi.varnier@gmail.com>
2022-09-09 10:44:18 +0000

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# a script that starts a Flask web application
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """ print hello world """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ print HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """display “C ” and free-text"""
+    return 'C ' + text.replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True)
