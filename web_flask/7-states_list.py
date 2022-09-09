5ec0b90959f473c2585226618ddbcb7830646f81
rominamarmontel <romi.varnier@gmail.com>
2022-09-09 19:22:03 +0000

    7-states_list.py

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module starts a Flask web application"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """The method lists all State"""
+    list_state = storage.all(State).values()
+    return render_template('7-states_list.html', list_state=list_state)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """The method remove the current SQLAlchemy Session"""
+    storage.close()
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5003)
+
+
+
+
