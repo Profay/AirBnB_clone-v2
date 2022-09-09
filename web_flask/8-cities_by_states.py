62a76719806cdf5d7b94b6bea13c017d529563a7
rominamarmontel <romi.varnier@gmail.com>
2022-09-09 19:41:18 +0000

    8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""a script that starts a Flask web application"""
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """The method list all cities of a State"""
+    list_state = storage.all(State).values()
+    return render_template('8-cities_by_states.html',
+                           list_states=list_state)
+
+
+@app.teardown_appcontext
+def tear_down(exception):
+    """The method remove the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", post=5003)
