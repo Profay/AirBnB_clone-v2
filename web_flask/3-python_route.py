ee7a2426685951da0530eec909c0dea0b9502b64
rominamarmontel <romi.varnier@gmail.com>
2022-09-10 10:33:17 +0000

    3-python_route.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""a script that starts a Flask web application"""
+"""a script that starts a Flask web application port 5000"""
 
 from flask import Flask
 
@@ -32,4 +32,4 @@ def pythoniscool(text='is cool'):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5003)
+    app.run(host="0.0.0.0", port=5000)
