9b006e0fc036be0ce2ed8afedba6cc1a92d405f3
rominamarmontel <romi.varnier@gmail.com>
2022-09-10 10:55:26 +0000

    debug

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,19 +2,18 @@
 """a script that starts a Flask web application"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """ print hello world """
+    """ display hello world """
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ print HBNB """
+    """ display HBNB """
     return "HBNB"
 
 
@@ -25,4 +24,4 @@ def cisfun(text):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
