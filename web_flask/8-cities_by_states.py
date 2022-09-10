821aa7a46513e2f55234d0f96855fa7df4b60ccd
rominamarmontel <romi.varnier@gmail.com>
2022-09-10 15:16:18 +0000

    8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """The method list all cities of a State"""
+    """ display the list cities of a State"""
     list_state = storage.all(State).values()
     return render_template('8-cities_by_states.html',
                            list_states=list_state)
@@ -24,4 +24,4 @@ def teardown(exception):
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", post=5003)
+    app.run(host="0.0.0.0", post=5000, debug=True)
