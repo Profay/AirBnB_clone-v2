772054ed27e8f0d26350982e0da1a6ce20df7533
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-28 18:48:38 +0000

    set up web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-# Script that deploy hbnb_static web server
-if [ ! -x /usr/sbin/nginx ]; then
-    sudo apt-get -y update
-    sudo apt-get -y install nginx
-    sudo service nginx start
-fi
-
+# Script that configures Nginx server with some folders and files
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo service nginx start
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -R ubuntu:ubuntu /data
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-enabled/default
-
+sudo echo "Holberton School" > /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit 0
