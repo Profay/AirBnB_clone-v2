281372c05241914eb5b65cb6b782f4f0ab001fd8
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-28 18:25:55 +0000

    close the brace

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -11,6 +11,6 @@ sudo mkdir -p /data/web_static/shared/
 sudo chown -R $USER:$USER /data
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t' /etc/nginx/sites-enabled/default
+sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
