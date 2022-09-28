07ea1d5713d9d6bf4ffc582b755d8545e878e1c4
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-28 18:53:30 +0000

    set up web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
-# Script that configures Nginx server with some folders and files
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo service nginx start
+# Script that deploy hbnb_static web server
+if [ ! -x /usr/sbin/nginx ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+    sudo service nginx start
+fi
+
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
+sudo chown -R ubuntu:ubuntu /data/
 sudo echo "Holberton School" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-enabled/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
 exit 0
