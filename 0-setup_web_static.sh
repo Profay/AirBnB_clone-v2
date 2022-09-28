8fd06fcc3b69a7975dc63da7dd758ab46a937a5c
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-28 18:37:52 +0000

    change owner ND GROUP

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # Script that deploy hbnb_static web server
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo service nginx start
+if [ ! -x /usr/sbin/nginx ]; then
+    sudo apt-get -y update
+    sudo apt-get -y install nginx
+    sudo service nginx start
+fi
 
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-sudo chown -R $USER:$USER /data
+sudo chown -R ubuntu:ubuntu /data
 echo "Holberton School" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-enabled/default
