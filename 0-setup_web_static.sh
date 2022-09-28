40442edadcbd20c8b1388874cbd79046ecf43354
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-28 18:42:59 +0000

    set up web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,3 +14,4 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
+exit 0
