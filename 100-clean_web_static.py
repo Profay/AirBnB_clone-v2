873c6901b6d7e6b831fb98efffdb1be3ea1d0828
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-29 20:36:21 +0000

    hh

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,14 +9,6 @@ env.user = "ubuntu"
 
 def do_clean(number=0):
     """ CLEANS """
-
-    number = int(number)
-
-    if number == 0:
-        number = 2
-    else:
-        number += 1
-
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
+    local('cd versions ; ls -t | head -n -{} | xargs rm -rf'.format(number))
     path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    run('cd {} ; ls -t | head -n -{} | xargs rm -rf'.format(path, number))
