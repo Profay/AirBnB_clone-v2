6f42480c6471424e4852e72f7df2fbdb09c6f856
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-29 08:51:48 +0000

    set up web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,56 +3,25 @@
 
 from os.path import exists
 from fabric.api import put, run, env
-env.hosts = ['3.91.63.125', '3.239.2.120']
-env.user = "ubuntu"
+env.hosts = ['50.19.193.215', '54.227.31.122']
 
 
 def do_deploy(archive_path):
     """function do_deploy"""
-
-    path = '/data/web_static/releases/'
-    """spliting the archived path at /.
-    removing versions dir example:
-    versions/web_static_20170315003959.tgz
-    """
-    file_name = archive_path.split('/')[1]
-
-    " remove .tgz "
-    no_extention = file_name.split('.')[0]
-
-    "Set destination path for unziping"
-    where_to_unzip = ('{}' + '{}/').format(path, no_extention)
-
-    "Set destination path for storing zipped file"
-    tmp_path = ('/tmp/{}').format(file_name)
-
     if not exists(archive_path):
         return False
     try:
-        "Store zipped file in /tmp/"
+        path = '/data/web_static/releases/'
+        file_name = archive_path.split('/')[1]
+        no_extention = file_name.split('.')[0]
         put(archive_path, '/tmp/')
-
-        "Create the destination directory"
-        run('mkdir -p {}'.format(where_to_unzip))
-
-        "Unzipped the zipped file to the directory created"
-        run('tar -xzf {} -C {}'.format(tmp_path, where_to_unzip))
-
-        "Remove the zipped file in /tmp/"
-        run('rm {}'.format(tmp_path))
-
-        "Transfer the file from <>/web_static/ to <> i.e rm /web_static/."
-        run('mv {}web_static/* {}'.format(where_to_unzip, where_to_unzip))
-
-        "Finally removing web_static after file transfer"
-        run('rm -rf {}web_static'.format(where_to_unzip))
-
-        "Remove the previous symbolic link"
+        run('mkdir -p {}{}/'.format(path, no_extention))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_extention))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_extention))
+        run('rm -rf {}{}/web_static'.format(path, no_extention))
         run('rm -rf /data/web_static/current')
-
-        "Create another symbolic link"
-        run('ln -s {} /data/web_static/current'.format(where_to_unzip))
-
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_extention))
         print('New version deployed!')
         return True
     except Exception:
