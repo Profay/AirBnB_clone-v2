ea405273725eb7278f2259b19973f604eb6bd65c
profytimmy <oyewaletimmy01@yahoo.com>
2022-09-29 21:01:04 +0000

    update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,15 +3,20 @@
 from fabric.api import *
 
 
-env.hosts = ['35.231.33.237', '34.74.155.163']
+env.hosts = ['3.91.63.125', '3.239.2.120']
 env.user = "ubuntu"
 
 
 def do_clean(number=0):
     """ CLEANS """
+
+    number = int(number)
+
+    if number == 0:
+        numbers = 1
+    else:
+        numbers = number
+
+    local('cd versions ; ls -t | head -n +{} | xargs rm -rf'.format(numbers))
     path = '/data/web_static/releases'
-    if number = 0
-        local('cd versions ; ls -t | head -n -1 | xargs rm -rf')
-        run('cd {} ; ls -t | head -n -1 | xargs rm -rf'.format(path))
-    local('cd versions ; ls -t | head -n -{} | xargs rm -rf'.format(number))
-    run('cd {} ; ls -t | head -n -{} | xargs rm -rf'.format(path, number))
+    run('cd {} ; ls -t | head -n +{} | xargs rm -rf'.format(path, numbers))
